baee19026abfc157853115cfbcd0050fc1f814f7
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-09 07:45:45 -0300

    chore: keydir code splitting to new module

---
+++ src/Keydir.hs
@@ -0,0 +1,27 @@
+module Keydir where
+
+import qualified Data.Map as Map
+
+import Caskfile (listCaskFiles, readEntries)
+import Entry (Entry (..), Checksum, FieldSize, Timestamp, Key)
+
+data KeydirEntry = KeydirEntry Checksum FieldSize Int Timestamp
+    deriving (Show, Eq)
+
+type Keydir = Map.Map Key KeydirEntry
+
+mapEntriesToKeydir :: [(Int, Entry)] -> Keydir
+mapEntriesToKeydir [] = Map.empty
+mapEntriesToKeydir ((offset, Entry fileid timestamp ksize vsize key value):entries) =
+    let entry = KeydirEntry 18 vsize offset timestamp in
+    Map.insert key entry $ mapEntriesToKeydir entries
+
+buildKeyDir :: IO Keydir
+buildKeyDir = do
+    caskfiles <- listCaskFiles
+    keydirs <- mapM (\caskfile -> do
+        entries <- readEntries caskfile
+        print entries
+        pure $ mapEntriesToKeydir entries
+        ) caskfiles
+    pure $ Map.unions keydirs
