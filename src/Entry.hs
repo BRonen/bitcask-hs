df4e19374e61550fdbdd60633c2f9da75795f77f
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-09 07:45:37 -0300

    chore: entry code splitting to new module

---
+++ src/Entry.hs
@@ -0,0 +1,54 @@
+module Entry where
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.UTF8 as BU
+
+import Data.Int (Int64)
+import Data.Digest.CRC32 (crc32)
+import Data.String.Interpolate
+import Data.Time (nominalDiffTimeToSeconds)
+import Data.Time.Clock.POSIX (getPOSIXTime)
+import Data.Word (Word32)
+
+type Checksum = Word32
+type Timestamp = Int64
+type FieldSize = Int64
+type Key = B.ByteString
+type Value = B.ByteString
+
+data Entry = Entry Checksum Timestamp FieldSize FieldSize Key Value
+    deriving (Eq)
+
+instance Show Entry where
+  show (Entry fileid timestamp ksize vsize key value) =
+    [i|#{fileid'} #{timestamp'} #{ksize'} #{vsize'} #{key'} #{value'}|]
+    where
+        fileid' = show fileid
+        timestamp' = show timestamp
+        ksize' = show ksize
+        vsize' = show vsize
+        key' = BU.toString key
+        value' = BU.toString value
+
+nanosSinceEpoch :: IO Timestamp
+nanosSinceEpoch = do
+    t <- getPOSIXTime
+    pure $ floor $ 1e9 * (nominalDiffTimeToSeconds t)
+
+buildEntry :: Timestamp -> Key -> Value -> Entry
+buildEntry timestamp key value = Entry checksum timestamp keyl valuel key value
+    where
+        checksum = crc32 $ BU.fromString $ show timestamp ++ show keyl ++ show valuel ++ key' ++ value'
+        keyl = B.length key :: FieldSize
+        valuel = B.length value :: FieldSize
+        key' = BU.toString key
+        value' = BU.toString value
+
+getEntryLength :: Entry -> Int
+getEntryLength (Entry _ _ ksize vsize _ _) = fromIntegral $ 4 + 8 + 8 + 8 + ksize + vsize
+
+{- 
+checkSum :: Checksum -> String -> String -> Bool
+checkSum checksum key value = checksum == checksum'
+    where
+        checksum' = crc32 $ BU.fromString $ key ++ value -}
