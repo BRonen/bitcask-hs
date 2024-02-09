bd00dd82a6f6c90734596ac01da1265a9aee831d
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-09 07:19:02 -0300

    feat: adding keydir builder

--- app/Main.hs
+++ app/Main.hs
@@ -44,7 +44,7 @@ deserialiseEntry = do
     value <- getLazyByteString vsize
     pure $ Entry checksum timestamp ksize vsize key value
 
-data KeydirEntry = KeydirEntry Int Int Int Int64
+data KeydirEntry = KeydirEntry Int Int64 Int Int64
     deriving (Show, Eq)
 
 type Keydir = Map.Map B.ByteString KeydirEntry
@@ -68,27 +68,29 @@ checkSum entryid key value = entryid == entryid'
     where
         entryid' = crc32 $ BU.fromString $ key ++ value
 
-getLastFileId :: IO Int
-getLastFileId = do
+listCaskFiles :: IO [FilePath]
+listCaskFiles = do
     currentdir <- getCurrentDirectory
     let workdir = currentdir ++ "/temp"
     workfiles <- listDirectory workdir
-    let files = filter (\workfile -> takeExtension workfile == ".cask") workfiles
-    case files of
+    case filter (\workfile -> takeExtension workfile == ".cask") workfiles of
+        [] -> pure []
+        files -> pure files
+
+getLastFileId :: IO Int
+getLastFileId = do
+    caskfiles <- listCaskFiles
+    let filenames = map (read . takeBaseName) caskfiles
+    case filenames of
         [] -> pure 0
-        _ -> pure $ maximum $ map (read . takeBaseName) files
+        _ -> pure $ maximum filenames
 
 prependEntry :: Int -> Entry -> IO Entry
 prependEntry fileid entry = do
     let path = "temp/" ++ show fileid ++ ".cask"
     exists <- doesFileExist path
-    !contents <- if exists
-        then do
-            !contents <- B.readFile path
-            print contents
-            pure contents
-        else do
-            pure B.empty
+    !contents <- if exists then B.readFile path else pure B.empty
+    print contents
     B.writeFile path $ (serialiseEntry entry) <> contents
     pure entry
 
@@ -99,7 +101,7 @@ readEntry :: B.ByteString -> IO Entry
 readEntry content = do
     pure $ runGet deserialiseEntry content
 
-deserialiseWithOffset :: B.ByteString -> Int -> IO [Entry]
+deserialiseWithOffset :: B.ByteString -> Int -> IO [(Int, Entry)]
 deserialiseWithOffset content offset = do
     let content' = B.drop (fromIntegral offset) content
     if content' == B.empty
@@ -107,18 +109,41 @@ deserialiseWithOffset content offset = do
     else do
         entry <- readEntry content'
         entries <- deserialiseWithOffset content $ offset + (getEntryLength entry)
-        pure $ entry : entries
+        pure $ (offset, entry) : entries
 
-readEntries :: Int -> IO [Entry]
-readEntries fileid = do
-    let path = "temp/" ++ show fileid ++ ".cask"
-    content <- B.readFile path
+readEntries :: FilePath -> IO [(Int, Entry)]
+readEntries path = do
+    content <- B.readFile $ "temp/" ++ path
     deserialiseWithOffset content 0
+
+mapEntriesToKeydir :: [(Int, Entry)] -> Keydir
+mapEntriesToKeydir [] = Map.empty
+mapEntriesToKeydir ((offset, Entry fileid timestamp ksize vsize key value):entries) =
+    let entry = KeydirEntry 18 vsize offset timestamp in
+    Map.insert key entry $ mapEntriesToKeydir entries
+
+buildKeyDir :: IO Keydir
+buildKeyDir = do
+    caskfiles <- listCaskFiles
+    keydirs <- mapM (\caskfile -> do
+        entries <- readEntries caskfile
+        print entries
+        pure $ mapEntriesToKeydir entries
+        ) caskfiles
+    pure $ Map.unions keydirs
 
 main :: IO ()
 main = do
     currentFileid <- fmap (0+) getLastFileId
     t <- nanosSinceEpoch
-    a <- prependEntry currentFileid $ buildEntry t "hello" "world"
-    print $ show a
-    readEntries currentFileid >>= print . show
+    prependEntry currentFileid $ buildEntry t "hello12" "world1"
+    prependEntry currentFileid $ buildEntry t "hello2" "world24"
+    a <- prependEntry currentFileid $ buildEntry t "hello33" "world3"
+    print a
+    entries <- readEntries $ show currentFileid ++ ".cask"
+    mapM print entries
+    print $ mapEntriesToKeydir entries
+    
+    keydir <- buildKeyDir
+    print keydir
+    pure ()
