3db49a1507e606ed28df1443992a73b7c074984d
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-10 17:50:59 -0300

    feat: testing get and put from main interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,29 @@
 module Main (main) where
 
-import qualified Data.ByteString.Lazy.UTF8 as BU
-
-import Entry
+{- import Entry
 import Caskfile
-import Keydir
-import Serializable
+import Keydir -}
+
+import qualified Bitcask as BC
 
 main :: IO ()
 main = do
-    currentFileid <- getCurrentFileId
+    handle' <- BC.open "temp/" True
+    case handle' of
+        Right handle -> do
+            print "success"
+            BC.put handle "hello" "world"
+            entry <- BC.put handle "hello1" "world2"
+            case entry of
+                Right entry' -> do
+                    print entry'
+                    value <- BC.get handle "hello1"
+                    print value
+                Left err -> print err
+            BC.close handle
+        Left err -> print err
+
+    {- currentFileid <- getCurrentFileId
     t <- nanosSinceEpoch
     _ <- prependEntry currentFileid $ buildEntry t (BU.fromString "hello12") (BU.fromString "world1")
     _ <- prependEntry currentFileid $ buildEntry t (BU.fromString "hello2") (BU.fromString "world24")
@@ -21,4 +35,4 @@ main = do
     
     keydir <- buildKeyDir
     print keydir
-    pure ()
+    pure () -}
