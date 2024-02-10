3c0e84fbd95dcc10ad3fe3e8b6313860a69e01c8
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-10 17:52:23 -0300

    feat: adding get value from keydir method

--- src/Keydir.hs
+++ src/Keydir.hs
@@ -2,26 +2,38 @@ module Keydir where
 
 import qualified Data.Map as Map
 
-import Caskfile (listCaskFiles, readEntries)
-import Entry (Entry (..), Checksum, FieldSize, Timestamp, Key)
+import System.FilePath ((</>))
+import Caskfile (listCaskFiles, readEntries, getFileIdFromPath, readValueFromPos)
+import Entry (Entry (..), Checksum, FieldSize, Timestamp, Key, Value)
 
-data KeydirEntry = KeydirEntry Checksum FieldSize Int Timestamp
+data KeydirEntry = KeydirEntry FilePath FieldSize Int Timestamp
     deriving (Show, Eq)
 
 type Keydir = Map.Map Key KeydirEntry
 
-mapEntriesToKeydir :: [(Int, Entry)] -> Keydir
-mapEntriesToKeydir [] = Map.empty
-mapEntriesToKeydir ((offset, Entry _ timestamp _ vsize key _):entries) =
-    let entry = KeydirEntry 18 vsize offset timestamp in
-    Map.insert key entry $ mapEntriesToKeydir entries
+mapEntriesToKeydir :: FilePath -> [(Int, Entry)] -> Keydir
+mapEntriesToKeydir _ [] = Map.empty
+mapEntriesToKeydir filepath ((offset, Entry _ timestamp ksize vsize key _):entries) =
+    let ksize' = fromIntegral ksize in
+    let entry = KeydirEntry filepath vsize (ksize' + 4 + 8 + 8 + 8 + offset) timestamp in
+    Map.insert key entry $ mapEntriesToKeydir filepath entries
 
-buildKeyDir :: IO Keydir
-buildKeyDir = do
-    caskfiles <- listCaskFiles
+getValueFromKeydir :: Keydir -> Key -> IO (Maybe Value)
+getValueFromKeydir keydir key = do
+    let keydir' = Map.lookup key keydir
+    case keydir' of
+        Just (KeydirEntry filepath vsize offset _) -> do
+            value <- readValueFromPos filepath vsize $ fromIntegral offset
+            pure $ Just value
+        Nothing -> pure Nothing
+
+buildKeyDir :: FilePath -> IO Keydir
+buildKeyDir dirpath = do
+    caskfiles <- listCaskFiles dirpath
     keydirs <- mapM (\caskfile -> do
-        entries <- readEntries caskfile
+        let caskpath = dirpath ++ "/" ++ caskfile
+        entries <- readEntries caskpath
         print entries
-        pure $ mapEntriesToKeydir entries
+        pure $ mapEntriesToKeydir caskpath entries
         ) caskfiles
     pure $ Map.unions keydirs
