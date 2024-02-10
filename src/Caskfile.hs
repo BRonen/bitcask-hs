a3d061e529eb95600822143fa5d7e1b2fedf71ff
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-10 17:52:02 -0300

    feat: adding get value by offset method

--- src/Caskfile.hs
+++ src/Caskfile.hs
@@ -2,39 +2,41 @@ module Caskfile where
 
 import qualified Data.ByteString.Lazy as B
 
+import Data.Int (Int64)
+import Data.Binary.Get (getLazyByteString, runGet)
 import System.FilePath (takeBaseName, takeExtension)
 import System.Directory (getCurrentDirectory, listDirectory, doesFileExist)
 
 import Serializable (encode, decode)
-import Entry (Entry (..), getEntryLength)
+import Entry (Entry (..), Value, getEntryLength)
 
-listCaskFiles :: IO [FilePath]
-listCaskFiles = do
-    currentdir <- getCurrentDirectory
-    let workdir = currentdir ++ "/temp"
-    workfiles <- listDirectory workdir
+getFileIdFromPath :: FilePath -> Int
+getFileIdFromPath = read . takeBaseName
+
+listCaskFiles :: FilePath -> IO [FilePath]
+listCaskFiles dirpath = do
+    workfiles <- listDirectory dirpath
     case filter (\workfile -> takeExtension workfile == ".cask") workfiles of
         [] -> pure []
         files -> pure files
 
-getLastFileId :: IO Int
-getLastFileId = do
-    caskfiles <- listCaskFiles
-    let filenames = map (read . takeBaseName) caskfiles
-    case filenames of
+getLastFileId :: FilePath -> IO Int
+getLastFileId dirpath = do
+    caskfiles <- listCaskFiles dirpath
+    let fileIds = map getFileIdFromPath caskfiles
+    case fileIds of
         [] -> pure 0
-        _ -> pure $ maximum filenames
+        _ -> pure $ maximum fileIds
 
-getCurrentFileId :: IO Int
-getCurrentFileId = fmap (1+) getLastFileId
+getCurrentFileId :: FilePath -> IO Int
+getCurrentFileId dirpath = fmap (1+) (getLastFileId dirpath)
 
-prependEntry :: Int -> Entry -> IO Entry
-prependEntry fileid entry = do
-    let path = "temp/" ++ show fileid ++ ".cask"
-    exists <- doesFileExist path
-    !contents <- if exists then B.readFile path else pure B.empty
+prependEntry :: FilePath -> Entry -> IO Entry
+prependEntry filepath entry = do
+    exists <- doesFileExist filepath
+    !contents <- if exists then B.readFile filepath else pure B.empty
     print contents
-    B.writeFile path $ (encode entry) <> contents
+    B.writeFile filepath $ (encode entry) <> contents
     pure entry
 
 decodeWithOffset :: B.ByteString -> Int -> IO [(Int, Entry)]
@@ -48,6 +50,15 @@ decodeWithOffset content offset = do
         pure $ (offset, entry) : entries
 
 readEntries :: FilePath -> IO [(Int, Entry)]
-readEntries path = do
-    content <- B.readFile $ "temp/" ++ path
+readEntries filepath = do
+    content <- B.readFile filepath
     decodeWithOffset content 0
+
+readValueFromPos :: FilePath -> Int64 -> Int64 -> IO Value
+readValueFromPos filepath vsize offset = do
+    content <- B.readFile filepath
+    pure $ runGet (getLazyByteString vsize) (B.drop offset content)
+
+createCaskLock :: FilePath -> IO ()
+createCaskLock dirpath = do
+    B.writeFile (dirpath ++ "cask.lock") ""
