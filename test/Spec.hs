5a5996504b24bd99bd593b513a9b2b8b5fcb56d8
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-11 05:33:16 -0300

    tests: initial setup to unit testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+module Main (main) where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain $ testGroup "Root" [test1, test2]
+
+test1 :: TestTree
+test1 = testCase "2+2=4" $ (2 + 2 :: Integer) @?= (4 :: Integer)
+
+test2 :: TestTree
+test2 = testCase "7 is even" $ assertBool "Oops, 7 is odd" (odd (7 :: Integer))
