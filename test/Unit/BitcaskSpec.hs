d010938fe283d3b93b993c08e285c4d15f29cd8c
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-12 02:54:09 -0300

    tests: adding initial open and close methods test cases

--- test/Unit/BitcaskSpec.hs
+++ test/Unit/BitcaskSpec.hs
@@ -1,10 +1,48 @@
 module Unit.BitcaskSpec (test) where
 
-import Test.Tasty
-import Test.Tasty.HUnit
+import qualified Bitcask as BC
+
+import Test.Tasty ( testGroup, TestTree )
+import Test.Tasty.HUnit ( testCase, (@?=), assertFailure, Assertion )
+import Control.Exception (bracket)
+import System.Directory (createDirectory, removeDirectoryRecursive)
+
+withTemporaryDirectory :: FilePath -> (FilePath -> Assertion) -> Assertion
+withTemporaryDirectory tmpdir = bracket setUp tearDown
+    where
+        setUp = createDirectory tmpdir >> pure tmpdir
+        tearDown _ = removeDirectoryRecursive tmpdir
 
 test :: TestTree
-test = testGroup "Bitcask" [test2]
+test = testGroup "Bitcask module" [testClose, testOpen]
+
+testOpen :: TestTree
+testOpen = testGroup "Open function" [test1]
+
+testClose :: TestTree
+testClose = testGroup "Close function" [test2]
+
+test1 :: TestTree
+test1 = testCase "Should error on attempt to instance a second writer" $
+    withTemporaryDirectory "temp1" $ \tmpDir -> do
+        firsthandle <- BC.open tmpDir True
+        case firsthandle of
+            Left _ -> assertFailure "Unable to open one bitcask handle"
+            Right _ -> do
+                secondhandle <- BC.open tmpDir True
+                case secondhandle of
+                    Right _ -> assertFailure "Able to instance two writers at same time"
+                    Left err -> err @?= "Unable to lock file"
 
 test2 :: TestTree
-test2 = testCase "7 is even" $ assertBool "Oops, 7 is odd" (odd (7 :: Integer))
+test2 = testCase "Should instance a second writer after close first" $
+    withTemporaryDirectory "temp2" $ \tmpDir -> do
+        firsthandle <- BC.open tmpDir True
+        case firsthandle of
+            Left _ -> assertFailure "Unable to open one bitcask handle"
+            Right firsthandle' -> do
+                BC.close firsthandle'
+                secondhandle <- BC.open tmpDir True
+                case secondhandle of
+                    Right _ -> pure ()
+                    Left err -> assertFailure err
