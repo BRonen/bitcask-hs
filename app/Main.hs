e769e0d04b3194ee100dee2574487a015c5057eb
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-12 02:55:34 -0300

    refactor: linting

--- app/Main.hs
+++ app/Main.hs
@@ -4,27 +4,27 @@ import qualified Bitcask as BC
 
 main :: IO ()
 main = do
-    handle' <- BC.open "temp/" True
-    print handle'
-    case handle' of
-        Left err -> print err
-        Right handle -> do
-            {- BC.put handle "hello3" "world3"
-            _ <- BC.delete handle "hello"
-            case entry of
-                Right entry' -> do
-                    print entry'
-                Left err -> print err -}
-            BC.get handle "hello" >>= print
-            BC.put handle "hello" "hello world" >>= print
-            BC.listKeys handle >>= print
-            BC.close handle
-            {- handle'' <- BC.merge handle
-            case handle'' of
-                Left err -> print err
-                Right handle''' -> do
-                    BC.listKeys handle''' >>= print
-                    BC.put handle''' "joao castro" "grande"
-                    BC.put handle''' "joao castro1" "grande2"
-                    BC.close handle''' -}
-            pure ()
+  handle' <- BC.open "temp/" True
+  print handle'
+  case handle' of
+    Left err -> print err
+    Right handle -> do
+      {- BC.put handle "hello3" "world3"
+      _ <- BC.delete handle "hello"
+      case entry of
+          Right entry' -> do
+              print entry'
+          Left err -> print err -}
+      BC.get handle "hello" >>= print
+      BC.put handle "hello" "hello world" >>= print
+      BC.listKeys handle >>= print
+      BC.close handle
+      {- handle'' <- BC.merge handle
+      case handle'' of
+          Left err -> print err
+          Right handle''' -> do
+              BC.listKeys handle''' >>= print
+              BC.put handle''' "joao castro" "grande"
+              BC.put handle''' "joao castro1" "grande2"
+              BC.close handle''' -}
+      pure ()
